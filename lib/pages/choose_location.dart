a709fb86a5348b88ffed97158db19a442d23caed
Ezekiel <ezekielphlat@gmail.com>
2020-08-15 22:14:12 +0100

    Add asynchronous request to loading page

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -6,10 +6,43 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  int counter = 0;
+  void getData() async{
+    // simulate network request for a username
+   await Future.delayed(Duration(seconds: 3), () {
+      print('Yoshi');
+    });
+
+    // simulate network request for a boi
+    Future.delayed(Duration(seconds: 2), () {
+      print('Biography of Yoshi');
+    });
+
+    print('statement');
+  }
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Choose Location Screen'),
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title:Text('Choose a Location'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: RaisedButton(
+        onPressed: (){
+          setState(() {
+            counter += 1;
+          });
+        },
+        child: Text('Counter is: $counter'),
+      ),
     );
   }
 }
